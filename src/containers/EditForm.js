b7bf2ecb841ba46e65aeefbad8b668109398e633
marshallhelenm <marshallhelenm@gmail.com>
2019-10-24 12:50:26 -0700

    breadcrumbs need work

--- src/containers/EditForm.js
+++ src/containers/EditForm.js
@@ -14,51 +14,6 @@ class EditForm extends Component {
       "Edit"
     );
   }
-  // state = {
-  //   fields: 0,
-  //   urls: [""]
-  // };
-
-  // printUrls = () => {
-  //   console.log("editform state: ", this.state);
-  //   let urlstring = "";
-  //   let urls = this.state.urls;
-  //   urls.forEach(url => {
-  //     urlstring === "" ? (urlstring += url) : (urlstring += `, ${url}`);
-  //   });
-  //   return urlstring;
-  // };
-
-  // generateURLFields = () => {
-  //   let i;
-  //   let fields = [];
-  //   for (i = 0; i <= this.state.fields; i++)
-  //     fields.push(
-  //       <Form.Field key={`img-field-${i}`}>
-  //         <Input
-  //           onChange={this.handleURLChange}
-  //           type="text"
-  //           placeholder="Image URL"
-  //           className="url_field"
-  //         />
-  //       </Form.Field>
-  //     );
-  //   return fields;
-  // };
-
-  // addUrlField = e => {
-  //   e.preventDefault();
-  //   let newfields = this.state.fields+1
-  //   console.log('----EditForm state: ', this.state)
-  //   this.setState(prevState => ({
-  //     fields: newfields
-  //   }));
-  // };
-
-  // handleURLChange = e => {
-  //   this.setState({ urls: [...this.state.urls, e.target.value] });
-  // };
-
   render() {
     console.log("Edit Form Props: ", this.props);
     return (
