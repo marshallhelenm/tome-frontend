64d24f94fd8a9bfb25645a469615c7e6539fb5b1
marshallhelenm <marshallhelenm@gmail.com>
2019-12-13 13:51:17 -0800

    wording

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -8,7 +8,7 @@ export default class UnderConstruction extends Component {
     return (
       <>
         <div className="content_section">
-          <h2>Sorry, the Tome is currently under some Construction! It will return after some modifications.</h2>
+          <h2>Sorry, the Tome is currently under Construction! It will return after some modifications.</h2>
           <h2><a href='https://www.loom.com/share/cf40bd77b2c548b99e52f2ab25bc42ee'>Click here for a brief demo video.</a></h2>
         </div>
         <div className="content_section login-page">
