da4314d7c569e15c1c3f36bdd5570c9b020f1e6c
marshallhelenm <marshallhelenm@gmail.com>
2020-04-06 09:55:56 -0700

    made sidebar logo a link and made its text black

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import '../css/notebook.css'
-import '../css/tooplate_style.css'
-import NavMenu from './navMenus/NavMenu.js'
+import "../css/notebook.css";
+import "../css/tooplate_style.css";
+import NavMenu from "./navMenus/NavMenu.js";
 
 class Sidebar extends Component {
   render() {
     return (
-            <div id='tooplate_sidebar'>
+      <div id="tooplate_sidebar">
+        <a href="https://wbtome.herokuapp.com/" id="logo-link">
+          <div id="logo">
+            <h3>The World Builder's</h3>
+            <h1>Tome</h1>
+          </div>
+        </a>
 
-                <div id='logo'>
-                  <h3>The World Builder's</h3>
-                    <h1>Tome</h1>
-                </div>
-
-                <NavMenu />
-
-            </div>
-      )
+        <NavMenu />
+      </div>
+    );
   }
 }
 
